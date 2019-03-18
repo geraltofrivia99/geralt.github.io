d6b6521abbf70b69cfd631bef18629962207e2f1
geraltofrivia99 <geralt.from.rivi9@yandex.ru>
2019-03-18 09:23:46 +0700

    Updates

---
+++ precache-manifest.8853b1c37f2b8d2af347ed093b470e17.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2baabfd4634859e695ecf131d169897b",
+    "url": "/geralt.github.io/static/media/1.2baabfd4.jpg"
+  },
+  {
+    "revision": "2956a9184ea4b31a6f8f",
+    "url": "/geralt.github.io/static/js/runtime~main.69ec703b.js"
+  },
+  {
+    "revision": "1b6586dcfa07a7b182a3",
+    "url": "/geralt.github.io/static/js/main.036a6170.chunk.js"
+  },
+  {
+    "revision": "c0f7825473edaf452f5d",
+    "url": "/geralt.github.io/static/js/5.9c4d68af.chunk.js"
+  },
+  {
+    "revision": "426b9d6bb651417289f4",
+    "url": "/geralt.github.io/static/js/4.fcd76cdf.chunk.js"
+  },
+  {
+    "revision": "24f4b06c739b09492c6d",
+    "url": "/geralt.github.io/static/js/3.df5acbb4.chunk.js"
+  },
+  {
+    "revision": "2f4b6786822a05a31cfb",
+    "url": "/geralt.github.io/static/js/2.05004b32.chunk.js"
+  },
+  {
+    "revision": "1b6586dcfa07a7b182a3",
+    "url": "/geralt.github.io/static/css/main.29271325.chunk.css"
+  },
+  {
+    "revision": "426b9d6bb651417289f4",
+    "url": "/geralt.github.io/static/css/4.7f21a385.chunk.css"
+  },
+  {
+    "revision": "63ffea5d956d229658cc054ed97b3504",
+    "url": "/geralt.github.io/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/geralt.github.io/precache-manifest.8853b1c37f2b8d2af347ed093b470e17.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/geralt.github.io/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
