067fb2f277b75fd775d92ed962556083ca298bce
Andriukh Nikita <nandriukh@allmax.team>
2019-03-18 08:59:41 +0700

    second

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 import Loadable from 'react-loadable';
 
 const AsyncHome = Loadable({
   loader: () => import('./pages/Home'),
   loading: () => <div>Loading...</div>,
-  delay: 300, // 0.3 seconds
+  delay: 300 // 0.3 seconds
 });
 
 const AsyncAbout = Loadable({
   loader: () => import('./pages/About'),
   loading: () => <div>Loading...</div>,
-  delay: 300, // 0.3 seconds
+  delay: 300 // 0.3 seconds
 });
 
 export default () => (
   <Switch>
-    <Route exact path="/" component={AsyncHome} />
-    <Route path="/about" component={AsyncAbout} />
+    <Route exact path="/more" component={AsyncHome} />
+    <Route exact path="/" component={AsyncAbout} />
   </Switch>
-)
+);
